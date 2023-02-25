2107facc28a1f8a2240c8242785480680859f96b
ishika18 <shagunphw@gmail.com>
2023-02-25 04:11:14 -0800

    first commit - get job info

---
+++ main.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome('./chromedriver')
+
+
+website_url = "https://www.linkedin.com/jobs/search"
+
+
+def scrape_linkedin_jobs():
+    # ------------------- Step 1 -----------------------
+    # go to the web page using selenium
+    driver.get(website_url)
+
+    print(driver.title)
+
+    # ------------------- Step 2 ------------------------
+    # get jobs -- main element
+
+    # search by class/id or any other field you can find
+    jobs = driver.find_elements(
+        value="base-card__full-link",
+        by=By.CLASS_NAME
+    )
+
+    # -------------------- Step 3 -----------------------
+    # get job info
+    for job in jobs:
+        print(job)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    scrape_linkedin_jobs()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
